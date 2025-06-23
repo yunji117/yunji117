cf00df89d6fa74e5ebba497ebce2a8fd04ffbf51
yunji117 <yunji117@users.noreply.github.com>
2025-06-23 10:31:38 +0000

    deploy: 01715440b19392d71213f07a3630f711cb1c2429

---
+++ src/components/thanks.tsx
@@ -0,0 +1,8 @@
+// src/components/Hero.tsx
+const Thanks = () => (
+  <section className="min-h-screen flex items-center justify-center">
+    <h1 className="text-6xl font-bold opacity-0 animate-hero-fade">Thank You •͜•</h1>
+  </section>
+);
+
+export default Thanks;
