5c91b7d477f9e71b6f577812ddeeed5f8e0672ac
yunji117 <yunji117@users.noreply.github.com>
2025-06-24 00:31:05 +0000

    deploy: a1f17d2b205bdea362eb7195904c64fb66035bab

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// 깃헙아이디/레포명/ 으로 설정!
 export default defineConfig({
   plugins: [react()],
+  base: '/yunji117.github.io/',
 })
