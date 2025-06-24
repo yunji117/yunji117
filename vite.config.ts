0cc3ad10c6e7330aa47a3b9ab5692160e9664fc9
김윤지 <yunw0117@gmail.com>
2025-06-24 09:34:54 +0900

    feat: vite.config.ts base 경로 수정

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 // 깃헙아이디/레포명/ 으로 설정!
 export default defineConfig({
   plugins: [react()],
-  base: '/yunji117.github.io/',
+  base: '/yunji117.github.io/yunji117',
 })
