856bd0d9f639fc180cd784c86a343fe4b2ae1c25
김윤지 <yunw0117@gmail.com>
2025-07-06 15:39:49 +0900

    빌드 및 수정

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/yunji117/',
+  build: {
+    outDir: 'docs', // 빌드 결과물을 docs 폴더에 생성
+  },
 })
