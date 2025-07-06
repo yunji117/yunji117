54038565ac677a00417634b1ddcda95d8a66c716
yunji117 <yunji117@users.noreply.github.com>
2025-07-06 06:40:13 +0000

    deploy: 856bd0d9f639fc180cd784c86a343fe4b2ae1c25

--- src/components/Hero.tsx
+++
@@ -1,8 +0,0 @@
-// src/components/Hero.tsx
-const Hero = () => (
-  <section id="hero" className="min-h-screen flex items-center justify-center">
-    <h1 className="text-6xl font-bold opacity-0 animate-hero-fade">KIM YUNJI</h1>
-  </section>
-);
-
-export default Hero;
--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-// vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// 깃헙아이디/레포명/ 으로 설정!
-export default defineConfig({
-  plugins: [react()],
-  base: '/yunji117/',
-})
